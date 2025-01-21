bc2b3219adfbd1487b6b5818103b00909ecb7b22
Luthfi Fathurrahman <98727237+luthfifathurrahman@users.noreply.github.com>
2025-01-22 00:55:55 +0700

    Add files via upload

---
+++ Day 1/Day 1.py
@@ -0,0 +1,25 @@
+# Print
+print("Hello World!")
+
+# Print with new line
+print("Hello World!\nHello Boss\nHello Bro")
+
+# Print combine string
+print("Hello" + "Luthfi")
+# The code above need a space
+print("Hello" + " " + "Luthfi")
+
+# asking for input
+input("What is your name?")
+
+# combine print with input
+print("hello " + input("What is your name?") + "!")
+
+# using input in variable
+name = input("what is your name?")
+print(name)
+
+# find how many character inside the name variable
+username = input("what is your username?")
+length = len(username)
+print(length)
